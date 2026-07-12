250f07b217b542f5d0ed8e1e3c1ae6f81dcbe238
abdulmaliksanni76-gif <abdulmaliksanni76@gmail.com>
2026-07-12 14:32:54 +0100

    vibeconnect-

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -6,6 +6,7 @@ const transporter = nodemailer.createTransport({
     port: 465, 
     secure: true, 
     family: 4,
+    connectionTimeout: 10000,
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
