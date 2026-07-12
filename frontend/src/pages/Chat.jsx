7595c5e166e74c83b4e9b120a23c48249364554e
abdulmaliksanni76-gif <abdulmaliksanni76@gmail.com>
2026-07-12 10:42:53 +0100

    vibeconnect-

--- frontend/src/pages/Chat.jsx
+++ frontend/src/pages/Chat.jsx
@@ -573,8 +573,8 @@ import axios from 'axios';
 import './Chat.css';
 import AudioPlayer from '../components/AudioPlayer';
 
-const BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:5000";
-// const BASE_URL = import.meta.env.VITE_API_URL || "";
+// const BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:5000";
+const BASE_URL = import.meta.env.VITE_API_URL || "";
 // const BASE_URL = import.meta.env.MODE === 'production' 
 //   ? "https://vibeconnect-1-f4m7.onrender.com" 
 //   : "http://localhost:5000";
